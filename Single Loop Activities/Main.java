4d736f59f03b58c68c7801234a71cb0fcb309d03
Richmond Baltazar <116050858+Retchizu@users.noreply.github.com>
2022-10-25 15:10:27 +0800

    Add files via upload

---
+++ Single Loop Activities/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		        
+		int number = 0,oddNumber = 1, oddNth = 1;
+        int nth = 0, sum = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number:");
+        number = scanner.nextInt();
+        nth = 2 * number - 1;
+        System.out.print("Odd numbers are ");
+
+        do{
+            if(oddNumber == 1){
+                System.out.print(oddNumber+ " ");
+                sum += oddNumber;
+            }
+            oddNumber += 2;
+            oddNth += 2;
+            sum += oddNumber;
+            System.out.print(oddNumber + " ");
+        }while(oddNth != nth);
+        System.out.println("\nThe sum of odd numbers up to " + number + " term is " + sum);
+		
+	}
+}
