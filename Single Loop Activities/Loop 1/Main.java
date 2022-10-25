42b2012b9755ea1caaca602603967d0ab4e1ef23
Richmond Baltazar <116050858+Retchizu@users.noreply.github.com>
2022-10-25 15:26:24 +0800

    Update Main.java

--- Single Loop Activities/Loop 1/Main.java
+++ Single Loop Activities/Loop 1/Main.java
@@ -7,8 +7,8 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		 int number = 0;
-		 int increase = 0;
+	     int number = 0;
+	     int increase = 0;
 	     Scanner scanner = new Scanner(System.in);
 	     System.out.println("Enter Number: ");
 	     number = scanner.nextInt();
