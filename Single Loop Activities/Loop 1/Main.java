51263de69138a32af0116ec367d3af1d0a445b98
Richmond Baltazar <116050858+Retchizu@users.noreply.github.com>
2022-10-25 15:25:56 +0800

    Add files via upload

---
+++ Single Loop Activities/Loop 1/Main.java
@@ -0,0 +1,22 @@
+//Richmond A. Baltazar
+//1BSCS-1
+//October 25, 2022
+
+
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		 int number = 0;
+		 int increase = 0;
+	     Scanner scanner = new Scanner(System.in);
+	     System.out.println("Enter Number: ");
+	     number = scanner.nextInt();
+	     
+	     while(number != 0){
+	        increase++;
+	        System.out.print(number + " " + increase + " ");
+	        number--;
+	     }
+	}
+}
